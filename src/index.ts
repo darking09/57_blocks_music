d2e903aab2f1924ed65e0137fa867b1bbf2fe755
Fernando Jose Torres Bermudez <fernandojoset@gmail.com>
2022-04-02 21:53:38 -0500

    add cluster to optimize the process

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
 // Dependecies
+import cluster from 'cluster';
 import app from "./app";
 import './database';
 
-app.listen(app.get('port'));
-console.log(`Server on port`, app.get('port'));
+const isProduction = process.env.NODE_ENV === 'production';
+
+if (isProduction) {
+  if (cluster.isMaster) {
+
+    // Count the machine's CPUs
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const os = require('os');
+    const cpuCount = os.cpus().length;
+
+    // Create a worker for each CPU
+    for (let i = 0; i < cpuCount; i += 1) {
+      cluster.fork();
+    }
+  } else {
+    app.listen(app.get('port'));
+    console.log(`Server on port`, app.get('port'));
+  }
+} else {
+  app.listen(app.get('port'));
+  console.log(`Server on port`, app.get('port'));
+}
